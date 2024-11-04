44ec5b622bf0094759813069432f48e1ceb2d7f5
Caterina Bosi <cbosi019@uottawa.ca>
2024-11-03 23:39:03 -0500

    Fixed bug

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,9 +8,13 @@ import {LoginComponent} from "./login/login.component";
 import {AdminComponent} from "./admin/admin.component";
 import {loggedInGuard} from "./logged-in.guard";
 import { AuthorsComponent } from './authors/authors.component';
+import { AuthorComponent } from './authors/author/author.component';
 
 const booksRoutes: Routes = [
   {path: ':id', component: BookComponent}
+];
+const authorsRoutes: Routes = [
+  {path: ':id', component: AuthorComponent},
 ];
 
 export const routes: Routes = [
@@ -26,7 +30,9 @@ export const routes: Routes = [
   {path: 'books', component: BooksComponent,
     children: booksRoutes
   },
-  {path: 'authors', component: AuthorsComponent},
+  {path: 'authors', component: AuthorsComponent,
+    children: authorsRoutes
+  },
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: '**', component: HomeComponent}
 ];
